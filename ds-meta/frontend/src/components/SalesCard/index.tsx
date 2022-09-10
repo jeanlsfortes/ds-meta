2e0a30d02efb24a5deec2070da2ceb701d6c6946
jeanls-solano <jeanlucaseh10@gmail.com>
2022-09-09 20:34:36 -0400

    Toast

--- ds-meta/frontend/src/components/SalesCard/index.tsx
+++ ds-meta/frontend/src/components/SalesCard/index.tsx
@@ -25,7 +25,7 @@ function SalesCard() {
         const dmin = minDate.toISOString().slice(0, 10);
         const dmax = maxDate.toISOString().slice(0, 10);
 
-        console.log(dmin);
+        //console.log(dmin);
 
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
 
@@ -33,7 +33,7 @@ function SalesCard() {
                 setSales(response.data.content);
             });
         
-        console.log(sales)
+        //console.log(sales)
     }, [minDate, maxDate]);
 
     return (
@@ -83,7 +83,7 @@ function SalesCard() {
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id} />
                                         </div>
                                     </td>
                                 </tr>
